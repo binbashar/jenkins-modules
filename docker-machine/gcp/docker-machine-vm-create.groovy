a3bde9b6a1c9ac6141fbedca49879315256434f1
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-04-16 01:55:51 -0300

    BBL-56 various modules docstring updates.

--- docker-machine/gcp/docker-machine-vm-create.groovy
+++ docker-machine/gcp/docker-machine-vm-create.groovy
@@ -1,69 +1,82 @@
 #!/usr/bin/env groovy
 /*
- * Jenkins Modules: docker-machine create with GCP drivers managing VM Size and public/private IP addr opt.
+ ** Jenkins Modules:
+ * docker-machine create with GCP drivers managing VM Size and public/private IP addr opt.
  *
- * Important: this module relies on docker and docker-machine, installed in the current jenkins server
+ ** IMPORTANT: this module relies on docker and docker-machine, installed in the current jenkins server
  * to be configured to run as-is, this module does not handle that.
  *
- * @param machineName       docker-machine name for the aws EC2 instancec.
- * @param projectId         --google-project: required The ID of your project to use when launching the instance.
- * @param machineUsername   --google-username: The username to use for the instance.
+ * This module has to be load as shown in the root context README.md
+*/
+/**
+ ** Function:
+ * Based on the docker-machine GPC input arguments an GCP VM will be created.
+ *
+ ** Parameters:
+ * @param String    dockerMachineName  docker-machine name for the GCP VM.
+ * @param String    projectId          --google-project: required The ID of your project to use when launching the instance.
+ * @param String    machineUsername    --google-username: The username to use for the instance.
+ * @param String    userInputVmSize    --google-disk-type: The disk type of instance.
+ * @param Integer   rootVmDiskSize     --google-disk-size: The disk size of instance.
+ *
+ ** Examples:
+ *     // Docker Machine GPC Size
+ *     String userInputVmSize = dockerMachineawsVmSize()
+ *
+ *     // Creating Docker-Machine on GPC
+ *     String dockerMachineName = "${userInputApp}-${date}-${appEnv}"
+ *     Integer rootVmDiskSize = 30
+ *     String projectId = cool-ocean-345643
+ *     String machineUsername = "jenkins"
+ *
+ *     dockerMachineGcp.call(userInputVmSize, dockerMachineName, rootVmDiskSize, machineUsername)
+ *
+ *     // or
+ *     // We can just run it with "externalCall(...)" since it has a call method.
+ *     dockerMachineGcp(userInputVmSize, dockerMachineName, rootVmDiskSize, machineUsername)
  */
-
-def call(machineName,machineUsername,projectId) {
-
-    def userInputVmSize
-    def userInputVmPubIp
+def call(String dockerMachineName, String machineUsername,String projectId, String userInputVmSize) {
+    
+    String userInputVmPubIp
 
     try {
-
-        stage('\u2708 Enter GCP machine size \u2705') {
-            userInputVmSize = input(
-                    id: 'userInputVmSize', message: 'Choose VM Size', ok: 'Submit', parameters: [
-                    [$class: 'ChoiceParameterDefinition', choices: 'f1-micro\n' +
-                            'g1-small\nn1-standard-1\nn1-standard-2', description: 'GCP VM Size:\n' +
-                            'f1-micro Effective hourly rate $0.006/hr ||\n' +
-                            'g-1small $0.019/hr ||\n' +
-                            'n1-standard-1 $0.034/hr ||\n' +
-                            'n1-standard-2 $0.067/hr ||\n' +
-                            'Billing Details: https://cloud.google.com/compute/pricing', name: 'target']
-            ])
-            echo "GCP Virtual Machine Size: ${userInputVmSize}"
-
-        }
-
+        
         stage('\u2708 Use Public IP Address? \u2705') {
             userInputVmPubIp = input(
                     id: 'userInputVmPubIp', message: 'Add Public IP Address?', ok: 'Submit', parameters: [
                     [$class: 'ChoiceParameterDefinition', choices: 'Yes\nNo', description: 'GCP VM Public Ip Addr', name: 'target']
             ])
 
+            userInputVmPubIp = userInputVmPubIp.toBoolean()
             echo "GCP VM it's going to use Public IP: ${userInputVmPubIp}"
         }
 
         stage("Create docker machine ${userInputVmSize} on GoogleCP") {
-            sh "#!/bin/bash \n" +
-                    "docker -v && -v && docker-machine -v"
+            //Validate docker OS pkg dependencies are satisfied.
+            sh """
+                #!/bin/bash
+                docker -v && docker-machine -v
+               """
 
             echo "GCP instance type: ${userInputVmSize}"
 
-            if (userInputVmPubIp == 'Yes'){
+            if (userInputVmPubIp){
                 sh "#!/bin/sh \n" +
                         "docker-machine create -d google --google-project ${projectId} --google-use-internal-ip --google-tags http-server,https-server " +
                         "--google-machine-image 'ubuntu-os-cloud/global/images/family/ubuntu-1604-lts' --google-machine-type ${userInputVmSize} " +
-                        "--google-zone 'us-east1-d' --google-username ${machineUsername} ${machineName}"
+                        "--google-disk-size ${rootVmDiskSize} --google-zone 'us-east1-d' --google-username ${machineUsername} ${dockerMachineName}"
             } else {
                 sh "#!/bin/sh \n" +
                         "docker-machine create -d google --google-project ${projectId} --google-use-internal-ip-only --google-tags http-server,https-server " +
                         "--google-machine-image 'ubuntu-os-cloud/global/images/family/ubuntu-1604-lts' --google-machine-type ${userInputVmSize} " +
-                        "--google-zone 'us-east1-d' --google-username ${machineUsername} ${machineName}"
+                        "--google-disk-size ${rootVmDiskSize} --google-zone 'us-east1-d' --google-username ${machineUsername} ${dockerMachineName}"
             }
 
-            sh "#!/bin/sh \n" +
-                    "docker-machine status ${machineName}"
+            sh "docker-machine status ${dockerMachineName}"
         }
 
     } catch (e) {
+        echo "[ERROR] Exception: ${e}"
         throw e as Throwable
     }
 }
