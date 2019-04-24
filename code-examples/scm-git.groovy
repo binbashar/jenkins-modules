53b165637764d72f8ac0516b2aac5be442c7124f
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-04-24 00:35:16 -0300

    BBL-56 new code examples in code-examples dir

--- code-examples/scm-git.groovy
+++ code-examples/scm-git.groovy
@@ -1,1 +1,113 @@
+#!/usr/bin/env groovy
 
+/////////////////////////////
+/*node {
+    // Load the file 'externalMethod.groovy' from the current directory, into a variable called "externalMethod".
+    def externalMethod = load("externalMethod.groovy")
+
+    // Call the method we defined in externalMethod.
+    externalMethod.lookAtThis("Steve")
+
+    // Now load 'externalCall.groovy'.
+    def externalCall = load("externalCall.groovy")
+
+    // We can just run it with "externalCall(...)" since it has a call method.
+    externalCall("Steve")
+}*/
+/////////////////////////////
+
+// USE CASE EXAMPLE
+// **Module:** Git Helper and Slack Helper modules.
+node {
+
+    // Jenkins Modules git checkout variables
+    String gitJenkinsModName = 'jenkins_modules'
+    String gitJenkinsModCredentialsId = 'jenkins_id_rsa'
+    String gitJenkinsModVersionTag = 'v0.0.1'
+    String gitJenkinsModRepoUrl = 'git@github.com:project/jenkins-modules.git'
+
+    // Load modules associated variables
+    def slackHelper
+    def gitHelper
+
+    // Examples related variables
+    String gitCommitId
+    String releaseTagPrefix = "release"
+    String lastChangesDiffMsg = ''
+
+    try {
+        stage ("Check-out Libraries") {
+            dir("${gitJenkinsModName}") {
+                checkout([
+                        $class           : "GitSCM",
+                        branches         : [[name: gitJenkinsModVersionTag]],
+                        userRemoteConfigs: [[
+                            credentialsId: gitJenkinsModCredentialsId,
+                            url: gitJenkinsModRepoUrl
+                        ]]
+                ])
+            }
+
+            slackHelper = load "${gitJenkinsModName}/slack/notification.groovy"
+            gitHelper = load "${gitJenkinsModName}/scm/git.groovy"
+        }
+
+        stage ("Notify Start") {
+            if (params.enableNotifications)
+                slackHelper.sendBuildStatus('STARTED')
+        }
+
+        stage ("Notify Last Changes") {
+            if (params.tagRelease) {
+                String releaseTagFilter = releaseTagPrefix + "_*"
+                String previousDeployCommitHash = gitHelper.getCommitHashByTag(releaseTagFilter)
+                String currentDeployCommitHash = gitCommitId
+
+                if (previousDeployCommitHash != '') {
+                    lastChangesDiffMsg = gitHelper.getDiffMessages(previousDeployCommitHash, currentDeployCommitHash)
+                } else {
+                    lastChangesDiffMsg = 'No Changes Found'
+                }
+
+                // Replace problematic characters in diff message
+                lastChangesDiffMsg = lastChangesDiffMsg.replace('"', '\'')
+
+                println "lastChangesDiffMsg: ${lastChangesDiffMsg}"
+                String previousTagName = gitHelper.getLastTagName(releaseTagFilter)
+                String lastChangesUrl = gitHelper.getRepositoryUrl() + "/compare/${previousTagName}...${currentDeployCommitHash}"
+                String lastChangesMsg = "Last Changes: <${lastChangesUrl}>  ```${lastChangesDiffMsg}```"
+
+                if (params.enableNotifications) {
+                    slackHelper.sendBuildStatus('LAST CHANGES', lastChangesMsg)
+                } else {
+                    println lastChangesMsg
+                }
+            }
+        }
+
+        stage ("Tag Release") {
+            if (params.tagRelease) {
+                String currentDeployCommitHash = gitCommitId
+                String tagDate = sh(returnStdout: true, script: 'date +%Y-%m-%d-%H-%M-%S').trim()
+                println "tagPrefix_date: ${releaseTagPrefix}_${tagDate}"
+                println "currentDeployCommitHash: ${currentDeployCommitHash}"
+                println "lastChangesDiffMsg: ${lastChangesDiffMsg}"
+
+                sh "git tag -a ${releaseTagPrefix}_${tagDate} ${currentDeployCommitHash} -m \"${lastChangesDiffMsg}\""
+                sh 'git push --tags'
+            }
+        }
+
+        stage ("Notify Success") {
+            if (params.enableNotifications)
+                slackHelper.sendBuildStatus('SUCCESS')
+        }
+
+    } catch (e) {
+        if (params.enableNotifications)
+            slackHelper.sendBuildStatus('FAILURE')
+
+        currentBuild.result = "FAILED"
+        throw e
+    }
+}
--- code-tests/scm_git_tests.groovy
+++ code-tests/scm_git_tests.groovy
@@ -1,1 +1,47 @@
+#!/usr/bin/env groovy
 
+// NOTE: File names have '_' instead of '-' because of the issue detailed in the link below:
+// https://stackoverflow.com/questions/36461121/groovy-calling-a-method-with-def-parameter-fails-with-illegal-class-name
+
+// TEST-1
+tagReleaseWithLastChanges("v0.0.1","v0.0.1","jenkins_test" )
+
+// FUNCTION-1
+def tagReleaseWithLastChanges(String releaseTagPrefix, String tagFilter, String tagMessage) {
+
+    String fromCommitHash = "987fe51cea0dfe547337c0bde706532795d778ae"
+    String currentDeployCommitHash = "6abc66c8b781466ce7dc89143bf6e7b2e78f2437"
+
+//    String tagDate = sh(returnStdout: true, script: 'date +%Y-%m-%d-%H-%M-%S').trim()
+    String dateCmd = 'date +%Y-%m-%d-%H-%M-%S'
+    String tagDate = executeCurlCommand(dateCmd)
+
+//    String lastChangesDiffMsg = getDiffMessages(fromCommitHash, currentDeployCommitHash)
+    lastChangesDiffMsg = "exequielrafaela (Tue Apr 23 11:30:13 2019): BBL-62 updating parameter section with return value for aws modules d6cf65e " +
+            "exequielrafaela (Tue Apr 16 01:54:50 2019): BBL-56 return values docstring added for several function modules 02dc27f " +
+            "exequielrafaela (Tue Apr 9 18:01:00 2019): BBL-56 several docstring and readme updates a6eed3a"
+
+    // Replace problematic characters in diff message
+    lastChangesDiffMsg = lastChangesDiffMsg.replace('"', '\'')
+
+    println "releaseTagPrefix_date: ${releaseTagPrefix}_${tagDate}"
+    println "fromCommitHash: ${fromCommitHash}"
+    println "currentDeployCommitHash: ${currentDeployCommitHash}"
+    println "lastChangesDiffMsg:\n ${lastChangesDiffMsg}"
+
+//    sh "git tag -a ${releaseTagPrefix}_${tagDate} ${currentDeployCommitHash} -m \"${tagMessage} ${lastChangesDiffMsg}\""
+    String gitTagCmd = "git tag -a ${releaseTagPrefix}_${tagDate} ${currentDeployCommitHash} -m \"${tagMessage} ${lastChangesDiffMsg}\""
+    println "gitTagCmd: ${gitTagCmd}"
+    executeCurlCommand(gitTagCmd)
+
+//    sh 'git push --tags'
+    String gitTagPush = 'git push --tags'
+    executeCurlCommand(gitTagPush)
+}
+
+def static executeCurlCommand(shCmd){
+    def proc = shCmd.execute()
+    def outputStream = new StringBuffer()
+    proc.waitForProcessOutput(outputStream, System.err)
+    return outputStream.toString().trim()
+}
